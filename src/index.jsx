402681931e1c967ac37c7ed751f1bb64275d9e42
Markéta Mížová <marketa.mizova@gmail.com>
2023-06-03 12:39:39 +0200

    using useState-onClick-setLikesUp/Down

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
-
-
 export const App = () => {
+  const [likesUp, setLikesUp] = useState(0);
+  const [likesDown, setLikesDown] = useState(0);
+ 
+
   return (
-    <>
+
     
     <div className="container">
     <div className="joke">
@@ -23,15 +25,15 @@ export const App = () => {
         </p>
       </div>
       <div className="joke__likes">
-        <button id="btn-up" className="btn-like btn-like--up"></button>
-        <span id="likes-up" className="likes-count likes-count--up">0</span>
-        <button id="btn-down" className="btn-like btn-like--down"></button>
-        <span id="likes-down" className="likes-count likes-count--down">0</span>
+        <button id="btn-up" className="btn-like btn-like--up" onClick={() => setLikesUp(likesUp + 1)}></button>
+        <span id="likes-up" className="likes-count likes-count--up">{likesUp}</span>
+        <button id="btn-down" className="btn-like btn-like--down" onClick={() => setLikesDown(likesDown +1)}></button>
+        <span id="likes-down" className="likes-count likes-count--down">{likesDown}</span>
       </div>
     </div>
   </div>
     
-    </>
+   
   );
 };
 
